ed740ee76c3c9012838db70330094dc114af1a4a
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-20 20:18:47 -0800

    added player tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper.rb'
+
+# now in this file we are just testing using the methods together so the tests will be longer
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Player do
+  before(:each) do
+    @player = Player.new(4, 4)
+  end
+
+  describe "#initialize" do
+    it "initializes" do
+      expect(@player).to be_instance_of(Player)
+    end
+
+    it 'has a board' do
+      expect(@player.board).to be_instance_of(Board)
+    end
+  end
+
+  it 'can add ships' do
+    @player.add_ship("Cruiser", 3)
+    expect(@player.ships.length).to eq(1)
+    expect(@player.ships.first.name).to eq("Cruiser")
+    expect(@player.ships.first.length).to eq(3)
+  end
+
+  it 'can place ships' do
+    @player.add_ship("Cruiser", 3)
+    allow(@player).to receive(:gets).and_return("A1 A2 A3\n")
+    expect { @player.place_ships }.to output.to_stdout
+  end
+
+  it 'can take a shot' do
+    opponent_board = Board.new(4, 4)
+    allow(@player).to receive(:gets).and_return("A1\n")
+    expect { @player.take_shot(opponent_board) }.to output.to_stdout
+  end
+
+  it 'can report shot result' do
+    opponent_board = Board.new(4, 4)
+    opponent_board.place(Ship.new("Cruiser", 3), ["A1", "A2", "A3"])
+    allow(@player).to receive(:gets).and_return("A1\n")
+    @player.take_shot(opponent_board)
+    expect(@player.shot_result(opponent_board, "A1")).to eq("\e[32mhit\e[0m")
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,6 @@ require './lib/cell.rb'
 require './lib/ship.rb'
 require './lib/board.rb'
 require './lib/game.rb'
+require './lib/player.rb'
+
+require 'rspec'

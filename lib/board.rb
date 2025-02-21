2762cb875112052fd6cee21278a4690a38af2622
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-20 17:34:07 -0800

    end of iteration 2

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 require './lib/cell'
+require './lib/ship'
 
 class Board
   def initialize
@@ -17,5 +18,45 @@ class Board
 
   def valid_coordinate?(coordinate)
     @cells.key?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    # if coordinates matches the length of the ship, then we can check if the coordinates are valid
+    return false unless coordinates.length == ship.length # still not sure if im allowed to use return unless yet.
+  
+    # this just gets the letters/numbers from the coords.
+    letters = coordinates.map { |coord| coord[0] } # no usage of proc syntax anywhere hehehe
+    numbers = coordinates.map { |coord| coord[1..-1].to_i }
+    # id love some feedback to possibly shorten this code
+  
+    # just checks if numbers/letters are the same
+    same_letter = letters.uniq.length == 1 # i know, fantanstic naming schemes all around.
+    same_number = numbers.uniq.length == 1
+  
+    consecutive_numbers = numbers.each_cons(2).all? { |a, b| b == a + 1 }
+    consecutive_letters = letters.each_cons(2).all? { |a, b| b.ord == a.ord + 1 }
+  
+    (same_letter && consecutive_numbers) || (same_number && consecutive_letters) # if this is true, then the ship is placed correctly
+  end
+
+  def place(ship, coordinates)
+    return unless valid_placement?(ship, coordinates) # gonna be using a lot of return unless statements
+ 
+    coordinates.each do |coordinate| # for each coordinate, place the ship
+      @cells[coordinate].place_ship(ship)
+    end
+  end
+
+  def render(show_ships = false)
+    board_string = "  1 2 3 4 \n"
+    ("A".."D").each do |letter|
+      row = "#{letter} "
+      (1..4).each do |number|
+        coordinate = "#{letter}#{number}"
+        row += @cells[coordinate].render(show_ships) + " "
+      end
+      board_string += row.strip + " \n" # i was wondering when id use "\n" to make a new line haha
+    end
+    board_string
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper.rb'
 
+require 'spec_helper.rb'
+
 RSpec.configure do |config|
   config.formatter = :documentation
 end
@@ -7,6 +9,8 @@ end
 RSpec.describe Board do
   before(:each) do
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   describe '#initialize' do
@@ -27,14 +31,61 @@ RSpec.describe Board do
     end
   end
 
-  describe '#valid_coordinate?' do # should i be making a def for each and everything?
-  # or just initialize? I am unsure of the standard practice.
+  describe '#valid_coordinate?' do
     it 'validates coordinates' do
       expect(@board.valid_coordinate?("A1")).to be true
       expect(@board.valid_coordinate?("D4")).to be true
       expect(@board.valid_coordinate?("A5")).to be false
       expect(@board.valid_coordinate?("E1")).to be false
       expect(@board.valid_coordinate?("A22")).to be false
+    end
+  end
+
+  describe '#valid_placement?' do
+    it 'validates length of ship' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
+    end
+
+    it 'consecutive coordinates' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false # just different style ships (2 long, 3 long etc just sanity checkin them)
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
+    end
+
+    it 'non-diagonal coordinates' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
+    end
+
+    it 'correct placement check' do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
+    end
+  end
+
+  describe '#place' do
+    it 'places a ship on the board' do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+
+      cell_1 = @board.cells["A1"]
+      cell_2 = @board.cells["A2"]
+      cell_3 = @board.cells["A3"]
+
+      expect(cell_1.ship).to eq(@cruiser)
+      expect(cell_2.ship).to eq(@cruiser)
+      expect(cell_3.ship).to eq(@cruiser)
+      expect(cell_3.ship).to eq(cell_2.ship)
+    end
+  end
+
+  describe '#render' do
+    it 'renders the board' do
+      expect(@board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      expect(@board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
     end
   end
 end

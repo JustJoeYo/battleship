f4d68e1f6ffc89a2a9cdb5b36ee79054a35719ba
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-24 08:52:58 -0800

    board spec check

--- runner.rb
+++ runner.rb
@@ -1,4 +1,8 @@
 require './lib/game'
 
+# went with new approach, dont stuff everything in runner, 
+# plus allows me to meet the rubric requirement of one extra class not outlined
+# in the project spec, which is the player class and the game class.
+
 game = Game.new
 game.start
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,60 +6,60 @@ end
 
 RSpec.describe Board do
   before(:each) do
-    @board = Board.new
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @board = Board.new # => #<Board:0x00007ff0728c8010...>
+    @cruiser = Ship.new("Cruiser", 3) # => #<Ship:0x00007fcb0d989510...>
+    @submarine = Ship.new("Submarine", 2) # => #<Ship:0x00007fcb0e8402c0...>
   end
 
   describe '#initialize' do
     it 'initialize' do
-      expect(@board).to be_an_instance_of(Board)
+      expect(@board).to be_an_instance_of(Board) # => true
     end
 
     it 'is a hash' do
-      expect(@board.cells).to be_a(Hash)
+      expect(@board.cells).to be_a(Hash) # => true
     end
 
     it 'has cells' do
-      expect(@board.cells.size).to eq(16)
+      expect(@board.cells.size).to eq(16) # => 16
     end
 
     it 'has values' do
-      expect(@board.cells.values.all? { |cell| cell.is_a?(Cell) }).to be true
+      expect(@board.cells.values.all? { |cell| cell.is_a?(Cell) }).to be true # => true
     end
   end
 
   describe '#valid_coordinate?' do
     it 'validates coordinates' do
-      expect(@board.valid_coordinate?("A1")).to be true
-      expect(@board.valid_coordinate?("D4")).to be true
-      expect(@board.valid_coordinate?("A5")).to be false
-      expect(@board.valid_coordinate?("E1")).to be false
-      expect(@board.valid_coordinate?("A22")).to be false
+      expect(@board.valid_coordinate?("A1")).to be true # => true
+      expect(@board.valid_coordinate?("D4")).to be true # => true
+      expect(@board.valid_coordinate?("A5")).to be false # => false
+      expect(@board.valid_coordinate?("E1")).to be false # => false
+      expect(@board.valid_coordinate?("A22")).to be false # => false
     end
   end
 
   describe '#valid_placement?' do
     it 'validates length of ship' do
-      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
-      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false # => false
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false # => false
     end
 
     it 'consecutive coordinates' do
-      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
-      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false
-      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
-      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false # => false
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false # => false
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false # => false
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false # => false
     end
 
     it 'non-diagonal coordinates' do
-      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
-      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false # => false
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false # => false
     end
 
     it 'correct placement check' do
-      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
-      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])). to be true
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true # => true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true # => true
     end
   end
 
@@ -71,10 +71,10 @@ RSpec.describe Board do
       cell_2 = @board.cells["A2"]
       cell_3 = @board.cells["A3"]
 
-      expect(cell_1.ship).to eq(@cruiser)
-      expect(cell_2.ship).to eq(@cruiser)
-      expect(cell_3.ship).to eq(@cruiser)
-      expect(cell_3.ship).to eq(cell_2.ship)
+      expect(cell_1.ship).to eq(@cruiser) # => #<Ship:0x00007fcb0d989510...>
+      expect(cell_2.ship).to eq(@cruiser) # => #<Ship:0x00007fcb0d989510...>
+      expect(cell_3.ship).to eq(@cruiser) # => #<Ship:0x00007fcb0d989510...>
+      expect(cell_3.ship).to eq(cell_2.ship) # => true
     end
   end
 
@@ -82,12 +82,12 @@ RSpec.describe Board do
     it 'renders the board' do
       # using expected_output to keep it slightly organized
       expected_output = "     1  2  3  4 \n  ┌────────────────┐\nA │   │   │   │   │\n  ├───┼───┼───┼───┤\nB │   │   │   │   │\n  ├───┼───┼───┼───┤\nC │   │   │   │   │\n  ├───┼───┼───┼───┤\nD │   │   │   │   │\n  └────────────────┘\n"
-      expect(@board.render.gsub(/\s+/, '')).to eq(expected_output.gsub(/\s+/, ''))
+      expect(@board.render.gsub(/\s+/, '')).to eq(expected_output.gsub(/\s+/, '')) # => true
       @board.place(@cruiser, ["A1", "A2", "A3"])
       # using gsub to remove spaces ".gsub(/\s+/, '')" we can check the output
       # is the same or not without worrying about the minor formatting differences.
       expected_output_2 = "     1  2  3  4 \n  ┌────────────────┐\nA │ \e[32mS\e[0m │ \e[32mS\e[0m │ \e[32mS\e[0m │   │\n  ├───┼───┼───┼───┤\nB │   │   │   │   │\n  ├───┼───┼───┼───┤\nC │   │   │   │   │\n  ├───┼───┼───┼───┤\nD │   │   │   │   │\n  └────────────────┘\n"
-      expect(@board.render(true).gsub(/\s+/, '')).to eq(expected_output_2.gsub(/\s+/, ''))
+      expect(@board.render(true).gsub(/\s+/, '')).to eq(expected_output_2.gsub(/\s+/, '')) # => true
     end
   end
 end
